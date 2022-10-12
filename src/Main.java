3510c8d6df26fe886a3ba9998bd9031612cd46d4
Pierre Gauthier <p.gauthier09@gmail.com>
2022-10-12 11:43:41 +0200

    init commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int longueur=500;
+        int largeur=250;
+        int perimetre=2*(longueur+largeur);
+        int surface=longueur*largeur;
+        System.out.println(perimetre);
+        System.out.println(surface);
+    }
+
+}
